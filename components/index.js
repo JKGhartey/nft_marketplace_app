30279f6122b29a80f48d48be9957a97ee9e593e3
Jerome Ghartey <gharteyjerome@gmail.com>
2023-07-30 21:38:57 +0100

    details screen

--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import { CircleButton, RectButton } from "./Button";
 import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
-export { FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RectButton };
+export {
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
--- screens/Details.js
+++ screens/Details.js
@@ -1,12 +1,101 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
 
-const Details = () => {
+import { COLORS, SIZES, assets, SHADOWS, FONTS } from "../constants";
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+  FocusedStatusBar,
+} from "../components";
+
+const DetailsHeader = ({ data, navigation }) => (
+  <View style={{ width: "100%", height: 420 }}>
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    />
+
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 55}
+    />
+
+    <CircleButton
+      imgUrl={assets.heart}
+      right={15}
+      top={StatusBar.currentHeight + 55}
+    />
+  </View>
+);
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
-  )
-}
+    <View style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 10,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255,255,255,0.5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
 
-export default Details
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: SIZES.extraLarge * 3,
+        }}
+        ListHeaderComponent={() => (
+          <>
+            <DetailsHeader data={data} navigation={navigation} />
+            <SubInfo />
+            <View style={{ pading: SIZES.font, marginHorizontal: 15 }}>
+              <DetailsDesc data={data} />
+              {data.bids.length > 0 && (
+                <Text
+                  style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary,
+                  }}
+                >
+                  Current Bid
+                </Text>
+              )}
+            </View>
+          </>
+        )}
+      />
+    </View>
+  );
+};
+
+export default Details;
